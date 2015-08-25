75c17e27c6b22fb3760e68cee967a024bc0de56d
Morten Henriksen <mh@gi2.dk>
2015-08-25 02:17:34 +0200

    1.2.4-rc.1 Meteor 1.2-rc.4 test

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'raix:localforage-localstorage',
-  version: '1.2.4',
+  version: '1.2.4-rc.1',
   summary: "Offline storage driver LocalStorage"
 });
 
 Package.on_use(function (api) {
-  // api.versionsFrom('1.2');
+  api.versionsFrom('METEOR@1.2-rc.4');
   api.use('ecmascript', 'client');
-  api.use('raix:localforage-serializer@1.2.4', 'client');
+  api.use('raix:localforage-serializer@1.2.4-rc.1', 'client');
   api.addFiles('localstorage.js', 'client');
   api.export('localStorageWrapper', 'client');
 });
